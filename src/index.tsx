1cd1fb9682195dd296c1c9592aa7fc21005eb400
PWojszko <piotr.wojszko@gmail.com>
2022-07-20 21:39:13 +0200

    React router basename

--- src/index.tsx
+++ src/index.tsx
@@ -19,7 +19,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={"https://pwojszko.github.io"}>
       <Provider store={store}>
         <AuthContextProvider>
           <RWDContextProvider>
